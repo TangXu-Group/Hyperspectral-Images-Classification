f3d4b668079e2dbcb71b9543b5bf579f2caf8866
yuki <yuki.666@qq.com>
2025-04-07 10:24:02 +0800

    update

--- SpiralMamba/readme.md
+++ SpiralMamba/readme.md
@@ -1,6 +1,25 @@
-## origins
+# SpiralMamba
 
-based on https://github.com/microsoft/Swin-Transformer#20240103
+This repository provides the code for the method in our paper '**SpiralMamba: Spatial-Spectral Complementary Mamba with Spatial Spiral Scan for Hyperspectral Image Classification**'. (TGRS2025)
 
-`main.py` and `utils/utils_ema.py` is modified from https://github.com/microsoft/Swin-Transformer#20240103, based on https://github.com/facebookresearch/ConvNeXt#20240103
+![本地路径](flowchart.png )
 
+**If you have any questions, you can send me an email. My mail address is yukiyao@stu.xidian.edu.cn.**
+
+## Datasets
+
+We conduct experiments on the Indian Pines, Pavia University, Houston2013 and Houston2018 datasets. To train and test our model, you should download the required data set and modify the corresponding parameters in *main.py* to meet your needs.
+
+## Requirements
+
+>python 3.10<br>
+>torch 2.1.1<br>
+>torchvision 0.16.1<br>
+>mamba-ssm 1.1.2<br>
+>causal-conv1d 1.1.1
+
+## Train
+By executing the following command, the experimental results can be obtained.
+```python
+sh run.sh
+```
--- SpiralMamba/train.sh
+++ SpiralMamba/train.sh
@@ -1,20 +1,7 @@
-python hsi-mamba-main.py --tao 0.5 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.5.log \
+python SpiralMamba-main.py --tao 0.95 --mixup_prob 0.05 --exp_name demo --dataset IP --patch_size 15 \
 && \
-python hsi-mamba-main.py --tao 0.55 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.55.log \
+python SpiralMamba-main.py --tao 0.9 --mixup_prob 0.05 --exp_name demo --dataset PU --patch_size 15 \
 && \
-python hsi-mamba-main.py --tao 0.6 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.6.log \
+python SpiralMamba-main.py --tao 0.7 --mixup_prob 0.3 --exp_name demo --dataset HUS --patch_size 11 \
 && \
-python hsi-mamba-main.py --tao 0.65 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.65.log \
-&& \
-python hsi-mamba-main.py --tao 0.7 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.7.log \
-&& \
-python hsi-mamba-main.py --tao 0.75 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.75.log \
-&& \
-python hsi-mamba-main.py --tao 0.85 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.85.log \
-&& \
-python hsi-mamba-main.py --tao 0.9 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.9.log \
-&& \
-python hsi-mamba-main.py --tao 0.95 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t0.95.log \
-&& \
-python hsi-mamba-main.py --tao 1 --exp_name 0809hui_specDilate_fusion --dataset IP --patch_size 15 >../log/IP/0809hui_specDilate_fusion_p15_T5_t1.log \
-
+python SpiralMamba-main.py --tao 0.85 --mixup_prob 0.05 --exp_name demo --dataset HUS18_100 --patch_size 17
